5e46bb50b267715fba7eb0c7538479ade378e332
lubnaasimz <lubnaa.mohamed@student.moringaschool.com>
2025-07-29 15:50:50 +0300

    home comp

--- car shopping/src/Page/home.jsx
+++ car shopping/src/Page/home.jsx
@@ -1,2 +1,13 @@
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>Find Your Perfect Vehicle</h1>
+          <p>Browse our extensive collection of quality pre-owned vehicles</p>
+          <Link to="/cars" className="browse-btn">Browse Cars</Link>
+        </div>
+      </div>
