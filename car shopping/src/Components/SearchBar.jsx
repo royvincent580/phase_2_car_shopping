026b48dfa01d752f224e96ac8f74967a9fc353d7
lubnaasimz <lubnaa.mohamed@student.moringaschool.com>
2025-07-29 15:25:06 +0300

    search components

--- car shopping/src/Components/SearchBar.jsx
+++ car shopping/src/Components/SearchBar.jsx
@@ -1,2 +1,12 @@
 import { useState } from 'react';
 import '../styles/SearchBar.css';
+
+
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
+  
